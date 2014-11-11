c0f75ade562683038bc832a0cf62bda38ee9df6c
JohnFreddy <jh0n.fr3ddym@gmail.com>
2014-11-11 13:23:23 -0500

    Seguridad 2.0

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+	def mostrar(ro)
+		if ro == 1
+			'<span class="label label-primary">Administrador</span>'.html_safe
+		else 
+			'<span class="label label-default">Usurio</span>'.html_safe
+		end
+	end
 end
