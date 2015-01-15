7bad00cf7a16691a102ef689a87d8dad00cdafe3
johnfreddy <jh0n.fr3ddym@gmail.com>
2015-01-15 15:00:49 +0000

    problema de imagenes en la nube resuelto

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   validates :imagen, :attachment_presence => true
   belongs_to :tela
-   has_attached_file :imagen, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  validates_attachment_content_type :imagen, :content_type => /\Aimage\/.*\Z/
+   has_attached_file :imagen
+  do_not_validate_attachment_file_type :imagen
 end
